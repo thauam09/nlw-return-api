c1be31a0117ee4b19134c7f8a92358bd6782c6c3
thauam09 <thauam_09martins@hotmail.com>
2022-05-09 00:25:46 -0300

    ajuste porta atraves variavel ambiente

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => {
+app.listen(process.env.port || port, () => {
   console.log(`HTTP server running in port ${port}!`);
 });
